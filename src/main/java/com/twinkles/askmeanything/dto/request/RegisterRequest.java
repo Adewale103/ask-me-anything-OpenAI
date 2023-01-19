6e587f644dbcd16f46b7f7880c84e38a7d302f8b
Adewale Adeyinka <adeyinkawale13@gmail.com>
2023-01-19 12:42:17 +0100

    added register dto

---
+++ src/main/java/com/twinkles/askmeanything/dto/request/RegisterRequest.java
@@ -0,0 +1,17 @@
+package com.twinkles.askmeanything.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/twinkles/askmeanything/dto/response/RegisterResponse.java
@@ -0,0 +1,12 @@
+package com.twinkles.askmeanything.dto.response;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RegisterResponse {
+    private String id;
+    private String email;
+}
