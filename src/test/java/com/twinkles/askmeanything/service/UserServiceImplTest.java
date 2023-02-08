661246d419333829819165a220a9811cc5320bd8
Adewale Adeyinka <adeyinkawale13@gmail.com>
2023-02-08 15:15:29 +0100

    working on user service test

--- src/test/java/com/twinkles/askmeanything/service/UserServiceImplTest.java
+++ src/test/java/com/twinkles/askmeanything/service/UserServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.twinkles.askmeanything.service;
 
 import com.twinkles.askmeanything.dto.request.RegisterRequest;
+import com.twinkles.askmeanything.model.User;
 import com.twinkles.askmeanything.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,23 +10,43 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
     private UserService userService;
     @Mock
     private UserRepository userRepository;
     private RegisterRequest registerRequest;
+    private  User user;
 
 
     @BeforeEach
     void setUp() {
         userService = new UserServiceImpl(userRepository);
-        registerRequest = RegisterRequest.builder().build();
+        registerRequest = RegisterRequest.builder()
+                .email("james@gmail.com")
+                .firstName("James")
+                .lastName("Fred")
+                .password("Nananaaa")
+                .build();
+       user = User.builder()
+                .id("1")
+                .email("james@gmail.com")
+                .firstName("James")
+                .lastName("Fred")
+                .password("Nananaaa")
+                .build();
     }
 
     @Test
     public void userCanRegisterTest(){
+        when(userRepository.findByEmail(any(String.class))).thenReturn(Optional.empty());
+        when(userRepository.save(any(User.class))).thenReturn(user);
 
     }
 }
